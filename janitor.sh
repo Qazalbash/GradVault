625b479b4554d5b913e25f312a63efbc4353f316
Meesum Ali Qazalbash <mesumali26.ma@gmail.com>
2023-04-29 15:42:46 +0500

    janotor working

--- janitor.sh
+++ janitor.sh
@@ -1,25 +1,13 @@
-# write a shell that will recursively go into each directory and delete all files that have name
-# .vscode
-# .git
-# .gitignore
-# .github
-# __pycache__
-# .idea
-
-# 1. find all files with the above names
-
-# 2. delete them
-
-# 3. if the file is a directory, go into it and repeat the process
-
-# start writing
-
-# it take one argument as the path to the directory
 janitor() {
-	for file in $(find SEM*); do
-		if [[ $file == *".vscode"* || $file == *".git"* || $file == *".gitignore"* || $file == *".github"* || $file == *"__pycache__"* || $file == *".idea"* ]]; then
-			echo "deleting $file"
-			rm -rvf $file
+	for dir in $(find SEM*); do
+		if [[ -d $dir ]]; then
+			# cd $dir
+			echo $(ls -a $dir)
+			for file in $(ls $dir -a); do
+				if [[ $file == *".vscode"* || $file == *".git"* || $file == *".gitignore"* || $file == *".github"* || $file == *"__pycache__"* || $file == *".idea"* ]]; then
+					rm -rvf $dir/$file
+				fi
+			done
 		fi
 	done
 }
