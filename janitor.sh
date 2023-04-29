223f879599cb6e2d87c9ea49c390629095be72cb
Meesum Ali Qazalbash <mesumali26.ma@gmail.com>
2023-04-29 15:45:42 +0500

    another try

--- janitor.sh
+++ janitor.sh
@@ -1,8 +1,6 @@
 janitor() {
 	for dir in $(find SEM*); do
 		if [[ -d $dir ]]; then
-			# cd $dir
-			echo $(ls -a $dir)
 			for file in $(ls $dir -a); do
 				if [[ $file == *".vscode"* || $file == *".git"* || $file == *".gitignore"* || $file == *".github"* || $file == *"__pycache__"* || $file == *".idea"* ]]; then
 					rm -rvf $dir/$file
